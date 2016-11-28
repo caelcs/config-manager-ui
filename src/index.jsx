b2fafe9125622842f117e50fe977913b4cd0d23c
caelwinner <adolfo.ernestocustidianosecchi@sky.uk>
2016-11-28 11:05:33 +0000

    #17: Adding awesome fonts and use for build config form

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/global.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
 import { render } from 'react-dom';
