f52aebccf16e5cecc53c94c860fcca8b8f802cca
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-01-19 22:00:35 +0000

    basic layout for one article

--- src/app/components/helpcentre/help_centre.jsx
+++ src/app/components/helpcentre/help_centre.jsx
@@ -3,6 +3,7 @@ import * as actions from '../../actions/index';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import {getOneArticle} from '../../reducers/articles';
+import Button from '@nowtv/nowtv-web-toolkit/src/react/components/Button/Button.react';
 
 const mapStateToProps = (state) => {
 	return {
@@ -22,8 +23,33 @@ class HelpCentre extends React.Component {
 		const {oneArticle} = this.props;
 		return (
 			<div>
-				<div>{oneArticle.title}</div>
-				<div>{oneArticle.content}</div>
+				<br />
+				<div className="n-container">
+						<div className="n-container__item">
+							<h3 className="n-bold n-primary-title">{oneArticle.title}</h3>
+						</div>
+						<div className="n-container__item">
+							<div className="n-body-text n-light">{oneArticle.content}</div>
+						</div>
+					<div>
+						<div className="n-container__item">
+							<p className="n-bold">Did you find this helpful?</p>
+							<Button colour="blue" href="#" disabled={false}>
+								Yes
+							</Button>
+							<Button colour="blue" href="#" disabled={false}>
+								No
+							</Button>
+						</div>
+						<div className="n-container__item">
+							<p className="n-bold">For more help on this topic
+								Contact Us</p>
+							<Button href="#" disabled={false}>
+								Contact Us
+							</Button>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
