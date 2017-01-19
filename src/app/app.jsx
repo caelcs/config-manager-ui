03b3cfe9bd569d6aa68cf88572f487a9f194e06f
caelwinner <adolfo.ernestocustidianosecchi@sky.uk>
2017-01-19 13:37:26 +0000

    making list of build config default

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
 		return (
 			<Router history={hashHistory}>
 				<Route path='/' component={MainLayout}>
-					<IndexRoute component={Home}/>
+					<IndexRoute component={BuildConfigContainer}/>
 					<Route path='/buildconfigs/home' component={BuildConfigContainer} />
 					<Route path='/buildconfigs/clone' component={BuildConfigClone} />
 					<Route path='/buildconfigs/add' component={BuildConfigAdd} />
